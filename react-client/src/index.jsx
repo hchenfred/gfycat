7ec5b0eec5df3509ad410439eb3a875861823f11
Huan Chen <hchen.fred@gmail.com>
2017-06-13 19:57:57 -0700

    (feature) integrate react-drawable-canvas

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,35 +2,55 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import DrawableCanvas from 'react-drawable-canvas';
 
 class App extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
+    this.state = {
+      brushColor: '#800909',
+      lineWidth: 4,
+      canvasStyle: {
+        backgroundColor: '#00FFDC'
+      },
+      clear: false
     }
+    this.handleOnClickClear = this.handleOnClickClear.bind(this);
+    this.handleOnClickChangeColorRed = this.handleOnClickChangeColorRed.bind(this);
+    this.handleOnClickChangeColorYellow = this.handleOnClickChangeColorYellow.bind(this);
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
+  handleOnClickClear() {
+    this.setState({
+      clear: true
     });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  handleOnClickChangeColorYellow() {
+    this.setState({
+      brushColor: '#ffff00',
+      clear: false
+    });
   }
-}
+
+  handleOnClickChangeColorRed(){
+    this.setState({
+      brushColor: '#800909',
+      clear: false
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <DrawableCanvas {...this.state} />
+        <button onClick={this.handleOnClickClear}>Clear all</button>
+        <button onClick={this.handleOnClickChangeColorYellow}>Set color to Yellow</button>
+        <button onClick={this.handleOnClickChangeColorRed}>Set color to Red</button>
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
