d8fdeae740812268f6d73e649812842b9f1ddb32
Huan Chen <hchen.fred@gmail.com>
2017-06-14 10:46:21 -0700

    (feat) add a done recording button to stop recording and a download button to download the generated gif

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -15,22 +15,58 @@ class App extends React.Component {
     this.state = {
       brushColor: '#800909',
       lineWidth: 4,
-      clear: false
+      clear: false,
+      accessToken: '',
+      grabbingFrames: false,
     }
     this.handleOnClickClear = this.handleOnClickClear.bind(this);
     this.handleOnClickChangeColorRed = this.handleOnClickChangeColorRed.bind(this);
     this.handleOnClickChangeColorYellow = this.handleOnClickChangeColorYellow.bind(this);
-    this.record = this.record.bind(this);
+    this.handleOnClickRecord = this.handleOnClickRecord.bind(this);
+    this.handleOnClickDownload = this.handleOnClickDownload.bind(this);
+    this.handleOnClickDone = this.handleOnClickDone.bind(this);
+    this.getGyfcatToken();   
+    this.encoder = new GIFEncoder();
+  }
+
+
+  getGyfcatToken() {
     fetch('https://api.gfycat.com/v1/oauth/token', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
       },
+      // TODO: save client_secret in a config file and gitignore it
       body: JSON.stringify({
         grant_type: "client_credentials",
         client_id: "2_qkXVfQ",
         client_secret: "HxndaZS-nqmEQw_QytRiqblQCYGHRajFJ-2ElodyvmMQWeRfsWP8Zas14NY5BQ1U",
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson.access_token);
+      this.setState({ accessToken: responseJson.access_token });
+        //return responseJson.movies;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  uploadToGyfcat(url, title) {
+    fetch('https://api.gfycat.com/v1/gfycats', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.state.accessToken}`,
+      },
+      // TODO: save client_secret in a config file and gitignore it
+      body: JSON.stringify({
+        fetchUrl: url,
+        title: title,
       })
     })
     .then((response) => response.json())
@@ -43,54 +79,48 @@ class App extends React.Component {
     });
   }
 
-  record() {
+  handleOnClickDownload() {
+    this.encoder.download();
+  }
+
+  handleOnClickDone() {
+    console.log('Finishing');
+    this.encoder.finish();
+    this.setState({ grabbingFrames: false });
+    const binary_gif = this.encoder.stream().getData();
+    const data_url = 'data:image/gif;base64,' + encode64(binary_gif);
+    this.uploadToGyfcat('', 'whatever');
+    const recordedImg = document.createElement('img');
+    recordedImg.setAttribute("src", data_url);
+    document.querySelector('.recordedGif').appendChild(recordedImg);
+  }
+
+  handleOnClickRecord() {
     document.querySelector('.recordedGif').innerHTML = '';
-    setTimeout(function() { // give external JS 1 second of time to load
     console.log('Starting');
-    var canvas = document.querySelector("canvas");
-    var context = canvas.getContext('2d');
+    const canvas = document.querySelector("canvas");
+    const context = canvas.getContext('2d');
     context.fillStyle = 'rgb(255,255,255)';
     context.fillRect(0,0,canvas.width, canvas.height); //GIF can't do transparent so do white
-    var shots  = [];
-    var grabLimit = 10;  // Number of screenshots to take
-    var grabRate  = 270; // Miliseconds. 500 = half a second
-    var count     = 0;
+    const grabRate  = 270; // Miliseconds. 500 = half a second
+    let count     = 0;
 
-    function showResults() {
-        console.log('Finishing');
-        encoder.finish();
-        var binary_gif = encoder.stream().getData();
-        var data_url = 'data:image/gif;base64,' + encode64(binary_gif);
-        //document.write('<img src="' +data_url + '"/>\n');
-        var recordedImg = document.createElement('img');
-        recordedImg.setAttribute("src", data_url);
-        //console.log(recordedImg);
-        document.querySelector('.recordedGif').appendChild(recordedImg);
-    }
+    this.encoder.start();
+    this.encoder.setRepeat(0);  //0  -> loop forever, 1+ -> loop n times then stop
+    this.encoder.setDelay(0); //go to next frame every n milliseconds
+    this.setState({ grabbingFrames: true });
 
-
-    var encoder = new GIFEncoder();
-    encoder.start();
-    encoder.setRepeat(0);  //0  -> loop forever, 1+ -> loop n times then stop
-    encoder.setDelay(0); //go to next frame every n milliseconds
-
-    var grabber = setInterval(function(){
+    const grabber = setInterval(() => {
+      if (!this.state.grabbingFrames) {
+        clearInterval(grabber);
+      }
       console.log('Grabbing '+count);
       count++;
-
-      if (count>grabLimit) {
-          clearInterval(grabber);
-          showResults();
-        }
-
-      encoder.addFrame(context);
-
-      }, grabRate);
-    }, 1000);
+      this.encoder.addFrame(context);
+    }, grabRate);
   }
 
   handleOnClickClear() {
-    
     this.setState({
       clear: true
     });
@@ -117,8 +147,10 @@ class App extends React.Component {
         <button onClick={this.handleOnClickClear}>Clear all</button>
         <button onClick={this.handleOnClickChangeColorYellow}>Set color to Yellow</button>
         <button onClick={this.handleOnClickChangeColorRed}>Set color to Red</button>
-        <button onClick={this.record}>Record to GIF</button>
-        <div className="recordedGif">hello</div>
+        <button onClick={this.handleOnClickRecord}>Start Recording</button>
+        <button onClick={this.handleOnClickDone}>Done Recording</button>
+        <button onClick={this.handleOnClickDownload}>Download the recorded GIF file</button>
+        <div className="recordedGif"></div>
       </div>
     );
   }
